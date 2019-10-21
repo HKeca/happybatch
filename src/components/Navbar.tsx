feddfaf546e4143db21b0d4ae922d040c4259c6d
Hunter Keca <hunterkeca@gmail.com>
2019-10-21 18:57:48 -0500

    Update navbar links, delete test class

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <h2>HappyBatch</h2>
+        <h2 className="navbar-header">HappyBatch</h2>
         <ul className="links">
-          <li>Browse</li>
-          <li>Favourites</li>
+          <li>
+            <Link className="navbar-link" to="/">
+              Browse
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-link" to="/favourites">
+              Favourites
+            </Link>
+          </li>
         </ul>
       </div>
     );
--- src/components/test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-class Test extends React.Component {
-  render() {
-    return <p>Hello, World!</p>;
-  }
-}
-
-export default Test;
